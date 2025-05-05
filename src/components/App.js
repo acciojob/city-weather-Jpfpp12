9c886954c215a667a0d801534e5cf96c76e38771
Jayprakash Sahu <118419073+Jpfpp12@users.noreply.github.com>
2025-05-05 11:21:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import 'regenerator-runtime/runtime'; // Add this line
+import 'regenerator-runtime/runtime';
 import './App.css';
 
 function App() {
